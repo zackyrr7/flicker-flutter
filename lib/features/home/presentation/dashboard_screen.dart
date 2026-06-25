b1d1817ca83c5976ca775a0386dbe4fbba044fab
zackyrr7 <zackyrr7@gmail.com>
2026-06-25 09:59:25 +0700

    feat:widget trending

--- lib/features/home/presentation/dashboard_screen.dart
+++ lib/features/home/presentation/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flicker/core/helper/responsive_space_extension.dart';
 import 'package:flicker/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
 
@@ -23,11 +24,32 @@ class DashboardScreen extends StatelessWidget {
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Trending Now',
+                        'TRENDING NOW',
                         style: Theme.of(context).textTheme.titleLarge?.copyWith(
                           color: AppColor.textWhiteColor,
                           fontWeight: FontWeight.bold,
                         ),
+                      ),
+                    ),
+                    Container(
+                      // child: ListView.builder(
+                      //   itemBuilder: (context, build) {
+                      //     return Container(
+                      //       width: 100,
+                      //       height: 100,
+                      //       decoration: BoxDecoration(color: Colors.amber),
+                      //     );
+                      //   },
+                      // ),
+                      height: MediaQuery.of(context).size.height * 0.15,
+
+                      color: Colors.amber,
+                      child: ListView.builder(
+                        itemCount: 10,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, build) {
+                          return Container();
+                        },
                       ),
                     ),
                   ],
